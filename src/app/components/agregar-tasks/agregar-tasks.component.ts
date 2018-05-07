d6ba6a4999b082b4cf63517522ad60326f927c35
googolplex <googolplex@lcompras.biz>
2018-05-07 16:24:57 +0000

    $

--- src/app/components/agregar-tasks/agregar-tasks.component.ts
+++ src/app/components/agregar-tasks/agregar-tasks.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+import { TareasfelicesService } from '../../services/tareasfelices.service' ;
+import { TareaFeliz } from '../../modelos/tareafeliz' ;
+
 
 @Component({
   selector: 'app-agregar-tasks',
   templateUrl: './agregar-tasks.component.html',
-  styleUrls: ['./agregar-tasks.component.css']
+  styleUrls: ['./agregar-tasks.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AgregarTasksComponent implements OnInit {
+  mitareafeliz : TareaFeliz = {
+   comandanro: 0 ,
+   nroruc:   0 ,
+   montototal: 0
+  };  
 
   constructor() { }
 
   ngOnInit() {
+  }
+  onSubmit() {
+    if(this.mitareafeliz.comandanro != 0 && this.mitareafeliz.nroruc != 0 && this.mitareafeliz.montototal != 0 {
+      this.TareasfelicesService.addTareaFeliz(this.mitareafeliz);
+      this.mitareafeliz.comandanro = 0 ;
+      this.mitareafeliz.nroruc = 0;
+      this.mitareafeliz.montototal = 0;
+    }
   }
 
 }
